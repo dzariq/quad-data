e72516e3252c29adc2e03c60fdc23ffe1ce53d02
dzariq mirza <dzariq85@gmail.com>
2023-11-20 14:57:26 +0800

    aa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    output: 'export',
+    distDir: '_static',
     images: {
-      domains: ["cdn.shopify.com"],
-    },
+      unoptimized: true
+    }
   };
 
 module.exports = nextConfig
