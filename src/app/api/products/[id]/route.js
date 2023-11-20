04d3ff5056d8980581429e9fe86db355baccae0c
dzariq mirza <dzariq85@gmail.com>
2023-11-20 19:26:03 +0800

    aa

--- src/app/api/products/[id]/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server"
-import data from '../../../../../data.json'
-
-export async function GET(request, context){
-    const {params} = context
-    const product = data.filter((x) => params.id == x.id.toString())
-
-    return NextResponse.json({product})
-}
