c42779103627581ac933100679a33c941b44c3b8
dzariq mirza <dzariq85@gmail.com>
2023-11-20 15:02:14 +0800

    aa

--- src/app/profile/[id]/post/[postId]/page.js
+++
@@ -1,7 +0,0 @@
-"use client"
-import { useParams } from "next/navigation"
-
-export default function ProfileIdPost(){
-    const params = useParams();
-    return <div>PROFILE POST ID {params.id} {params.postId}</div> 
- }
